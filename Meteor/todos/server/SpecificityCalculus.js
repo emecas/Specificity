5c654a4305992a145c4e3b900106c558ff7f8fef
Emerson Castaneda <emecas@hotmail.com>
2013-03-07 23:42:09 +0100

    Structuring Specificity calculus

---
+++ Meteor/todos/server/SpecificityCalculus.js
@@ -0,0 +1,21 @@
+
+
+function Lineal(collection){
+    return 1;
+}
+
+
+function LinealAB(collection){
+    return 2;
+}
+
+
+function Fractional(collection){
+    return 3;
+
+}
+
+
+function Product(collection){
+    return 4;
+}
